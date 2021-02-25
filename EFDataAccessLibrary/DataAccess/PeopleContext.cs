083a8459c8161bc808d1950a0229ef9ebeb9c69b
DS077375 <DS077375@cerner.com>
2021-02-25 17:29:09 -0600

    Set Up Frontend Connection

---
+++ EFDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,19 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        // Gives you a constructor if you want that calls the base constructor
+        public PeopleContext(DbContextOptions options): base(options) { }
+
+        // Create Tables (sets of Data)
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
