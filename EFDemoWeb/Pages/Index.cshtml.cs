5e0750d83de37a16a23142f771c6fd51a3899fc0
DS077375 <DS077375@cerner.com>
2021-02-26 08:31:00 -0600

    Where Clauses

--- EFDemoWeb/Pages/Index.cshtml.cs
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EFDemoWeb.Pages
@@ -11,15 +15,43 @@ namespace EFDemoWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
+            LoadSampleData();
+
+            // Get People link the addresses and email addresses and the result is a list of people
+            var people = _db.People
+                .Include(a => a.Addresses)
+                .Include(e => e.EmailAddresses)
+                .ToList();
+        }
+
+        private bool ApprovedAge(int age)
+        {
+            return (age >= 18 && age <= 65);
+        }
+
+
+        private void LoadSampleData()
+        {
+            if(_db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
 
         }
+
+
     }
 }
